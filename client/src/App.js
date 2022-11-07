59124f5060ce81d059111f631469c8a5c8335b51
jesse zenon <jesse.zenon@gmail.com>
2022-11-07 14:52:04 -0600

    login

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,23 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import NavBar from "./NavBar";
+import Login from "./Login/Login";
+import { Routes, Route } from "react-router-dom";
 import { getTopHeadlines } from "./api/news";
 
 function App() {
   const [topHeadlines, setTopHeadlines] = useState([]);
+  const [token, setToken] = useState("");
   useEffect(() => {
     getTopHeadlines().then((data) => setTopHeadlines(data.articles));
   }, []);
+  <Routes>
+    <Route path="/login" />
+  </Routes>;
 
+  if (!token) {
+    return <Login setToken={setToken} />;
+  }
   return (
     <div className="App">
       <NavBar {...{ topHeadlines }} />
--- client/src/api/news.js
+++ client/src/api/news.js
@@ -27,3 +27,13 @@ export const topHeadlineCategories = async (category) => {
   );
   return res.json();
 };
+
+export const loginUser = async (credentials) => {
+  return fetch("http://localhost:8080/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(credentials),
+  }).then((data) => data.json());
+};
